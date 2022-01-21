b10d9e6657bc39d8913f6727c90617b9f806945d
maisafranmendes <maisa.mendes@thoughtworks.com>
2022-01-21 15:51:20 -0300

    Add mapper, context and repository to Data project

---
+++ 05-Infra/Restaurante.Data/Mapeamentos/PratoMap.cs
@@ -0,0 +1,17 @@
+using Restaurante.Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Restaurante.Infra.Data.Mapeamentos
+{
+    public class PratoMap : MapBase<Prato>
+    {
+        public override void Configure(EntityTypeBuilder<Prato> builder)
+        {
+            base.Configure(builder);
+            builder.ToTable("prato");
+            builder.Property(c => c.Nome).IsRequired().HasColumnName("Nome").HasMaxLength(100);
+            builder.Property(c => c.Preco).IsRequired().HasColumnName("Preco");
+        }
+    }
+}
---
+++ 05-Infra/Restaurante.Data/Repositorios/PratoRepositorio.cs
@@ -0,0 +1,14 @@
+using Restaurante.Dominio.Entidades;
+using Restaurante.Dominio.Interfaces.Repositorios;
+using Restaurante.Infra.Data.Contextos;
+
+namespace Restaurante.Infra.Data.Repositorios
+{
+    public class PratoRepositorio : RepositorioBase<Prato>, IPratoRepositorio
+    {
+        public PratoRepositorio(Contexto contexto)
+            : base(contexto)
+        {
+        }
+    }
+}
