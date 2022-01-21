6a6c6951a169a12573fc022a51182cfe8e940e60
maisafranmendes <maisa.mendes@thoughtworks.com>
2022-01-21 11:40:31 -0300

    Add interface repo

---
+++ 04-Dominio/Restaurante.Dominio/Interfaces/Repositorios/IPratoRepositorio.cs
@@ -0,0 +1,12 @@
+using Restaurante.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurante.Dominio.Interfaces.Repositorios
+{
+    public interface IPratoRepositorio : IRepositorioBase<Prato>
+    {
+
+    }
+}
