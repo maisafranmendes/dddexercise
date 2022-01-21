893052bd42d2331a489e652f67401638e6b8c1e5
maisafranmendes <maisa.mendes@thoughtworks.com>
2022-01-21 11:32:02 -0300

    Add entities

---
+++ 04-Dominio/Restaurante.Dominio/Entidades/EntidadeBase.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurante.Dominio.Entidades
+{
+    public class EntidadeBase
+    {
+        public int Id { get; set; }
+    }
+}
